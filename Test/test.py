28056867f5ce3066de89a9bb4774ce92b64628ee
Nathalie Elias <nathaliemarie2303@gmail.com>
2022-06-15 19:55:58 -0400

    kata

--- Test/test.py
+++ Test/test.py
@@ -9,4 +9,4 @@ class testCases(unittest.TestCase):
     def test_getAll(self):
         self.assertEqual(rango.getAllPoints((2,True),(6,False)), [2,3,4,5])
     def test_Equals(self):
-        self.assertEqual(rango.equals((3,True),(5,False),(3,True),(5,False)))
+        self.assertEqual(rango.equals((3,True),(5,False),(3,True),(5,False) ),True)
