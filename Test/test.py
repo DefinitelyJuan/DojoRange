23449c957a5077a822ac586eb7bc631886b9a4fe
zolanovich <avilajuan0301@gmail.com>
2022-06-15 19:45:35 -0400

    getAll developed

--- Test/test.py
+++ Test/test.py
@@ -6,3 +6,7 @@ class testCases(unittest.TestCase):
         self.assertEqual(rango.contains((2,True),(6,False),[2,4]),True)
     def test_Contains2(self):
         self.assertEqual(rango.contains((2,True),(6,False),[-1,1,6,10]),False)
+    def test_getAll(self):
+        self.assertEqual(rango.getAllPoints((2,True),(6,False)), [2,3,4,5])
+    def test_Equals(self):
+        self.assertEqual(rango.equals((3,True),(5,False),(3,True),(5,False)))
--- kataRange.py
+++ kataRange.py
@@ -27,3 +27,30 @@ class rango:
                     return False                              
         else:
             return False
+
+    def getAllPoints(firstRange:tuple, secondRange:tuple):
+        numbers = []
+        if (firstRange[1] and secondRange[1] == False): 
+            for i in range(firstRange[0],secondRange[0]):
+                numbers.append(i)
+                
+        if (firstRange[1] and secondRange[1] == True):
+            for i in range(firstRange[0],secondRange[0]+1):
+                numbers.append(i)
+                
+        if (firstRange[1] == False and secondRange[1] == True):
+            for i in range(firstRange[0]+1,secondRange[0]+1):
+                numbers.append(i) 
+                                   
+        if (firstRange[1] == False and secondRange[1] == False): 
+            for i in range(firstRange[0]+1,secondRange[0]):
+                numbers.append(i)
+                            
+        return numbers
+
+    def equals(firstRange:tuple, secondRange:tuple, thirdRange:tuple, fourthRange:tuple):
+        if(firstRange == thirdRange and secondRange == fourthRange):
+            return True
+        else:
+            return False
+        
