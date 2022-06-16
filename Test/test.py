bd9b355aa364ef2d657b419beca317b240cd6df9
zolanovich <avilajuan0301@gmail.com>
2022-06-15 22:25:58 -0400

    endpoints and containsRange

--- Test/test.py
+++ Test/test.py
@@ -10,3 +10,12 @@ class testCases(unittest.TestCase):
         self.assertEqual(rango.getAllPoints((2,True),(6,False)), [2,3,4,5])
     def test_Equals(self):
         self.assertEqual(rango.equals((3,True),(5,False),(3,True),(5,False) ),True)
+    def test_endPoints(self):
+        self.assertEqual(rango.endpoints((2,True),(6,False)), (2,5))
+        self.assertEqual(rango.endpoints((2,True),(6,True)), (2,6))
+    def test_containsRange(self):
+        self.assertEqual(rango.containsRange((2,True),(5,False),[7,True],[10,False]), False)
+        self.assertEqual(rango.containsRange((2,True),(5,False),[3,True],[5,False]), False)
+        self.assertEqual(rango.containsRange((2,True),(5,False),[2,True],[10,False]), False)
+
+
--- kataRange.py
+++ kataRange.py
@@ -1,3 +1,6 @@
+from array import array
+
+
 class rango:
     def contains(firstRange:tuple, secondRange:tuple, numbers: list):
         if (firstRange[1] and secondRange[1] == False): #si es inclusivo "["
@@ -7,19 +10,19 @@ class rango:
                 else:
                     return False
                 
-        if (firstRange[1] and secondRange[1] == True): #si es inclusivo "["
+        if (firstRange[1] and secondRange[1] == True): 
             for num in numbers:
                 if (firstRange[0]<= num and secondRange[0] >= num):
                     return True
                 else:
                     return False                    
-        if (firstRange[1] == False and secondRange[1] == True): #si es inclusivo "["
+        if (firstRange[1] == False and secondRange[1] == True): 
             for num in numbers:    
                 if (firstRange[0]< num and secondRange[0] >= num):
                     return True    
                 else:
                     return False                                    
-        if (firstRange[1] == False and secondRange[1] == False): #si es inclusivo "["
+        if (firstRange[1] == False and secondRange[1] == False): 
             for num in numbers: 
                 if (firstRange[0]< num and secondRange[0] > num):
                     return True  
@@ -54,3 +57,48 @@ class rango:
         else:
             return False
         
+    def endpoints(firstRange:tuple, secondRange:tuple): 
+        endpoint = ()
+        if (firstRange[1] and secondRange[1] == False): 
+            endpoint = (firstRange[0], secondRange[0]-1)
+                
+        if (firstRange[1] and secondRange[1] == True):
+            endpoint = (firstRange[0], secondRange[0])
+                
+        if (firstRange[1] == False and secondRange[1] == True):
+            endpoint = (firstRange[0]+1, secondRange[0]) 
+                                   
+        if (firstRange[1] == False and secondRange[1] == False): 
+            endpoint = (firstRange[0]+1, secondRange[0]-1)
+        return endpoint
+    def containsRange(firstRange:tuple, secondRange:tuple, thirdRange:list, fourthRange:list):
+        
+        if(thirdRange[1]):
+            thirdRange[0] += 1
+        if(fourthRange[1]):
+            fourthRange[0] -= 1
+
+
+        if (firstRange[1] and secondRange[1] == False): #[)
+            if ((firstRange[0]<= thirdRange[0] and secondRange[0] > thirdRange[0]) and (firstRange[0]<= fourthRange[0] and secondRange[0] > fourthRange[0])):
+                return True
+            else:
+                return False
+                
+        if (firstRange[1] and secondRange[1] == True): #[]
+            if ((firstRange[0]<= thirdRange[0] and secondRange[0] >= thirdRange[0]) and (firstRange[0]<= fourthRange[0] and secondRange[0] >= fourthRange[0])):
+                return True
+            else:
+                return False                    
+        if (firstRange[1] == False and secondRange[1] == True):#(]
+            if ((firstRange[0]< thirdRange[0] and secondRange[0] >= thirdRange[0]) and (firstRange[0]< fourthRange[0] and secondRange[0] >= fourthRange[0])):
+                return True    
+            else:
+                return False                                    
+        if (firstRange[1] == False and secondRange[1] == False):#()
+            if ((firstRange[0]< thirdRange[0] and secondRange[0] > thirdRange[0]) and (firstRange[0]< fourthRange[0] and secondRange[0] > fourthRange[0])):
+                return True  
+            else:
+                return False                              
+        else:
+            return False
